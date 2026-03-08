74bf67b406b205805fc3b2abc60b1c126966c4e4
Kazuho1222 <destiny5914@gmail.com>
2026-03-08 16:39:49 +0900

    main.tsx fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { TaskProvider } from "./features/tasks/context/TaskProvider";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Root element not found");
+createRoot(rootElement).render(
 	<StrictMode>
 		<TaskProvider>
 			<App />
